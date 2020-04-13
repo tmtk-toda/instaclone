1b6a3e7f7a8d5c0d11ed7a78d4e25d30a1e683e6
tmtk <blindhurts.6001@gmail.com>
2020-04-13 14:01:46 +0900

    画像投稿機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :feeds 
   get 'sessions/new'
   root 'blogs#index'
   resources :sessions, only: [:new, :create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_072805) do
+ActiveRecord::Schema.define(version: 2020_04_13_022851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2020_04_12_072805) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
+  end
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
