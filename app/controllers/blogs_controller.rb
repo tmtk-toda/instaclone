5322a65075a3a136f77812b52decb982fe9b4f5b
tmtk <blindhurts.6001@gmail.com>
2020-04-17 17:11:59 +0900

    zzz

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,6 +18,7 @@ class BlogsController < ApplicationController
   end
   def show
     @blog = Blog.find(params[:id])
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
   end
   def edit
     @blog = Blog.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :favorites
   resources :feeds
   get 'sessions/new'
   root 'blogs#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :favorites, only: [:create, :destroy]
   resources :blogs do
     collection do
       post :confirm
