95c499dcb890ddf9fb1ac4feea926fcaf1860b9a
tmtk <blindhurts.6001@gmail.com>
2020-04-19 15:52:26 +0900

    メール機能修正２

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "facebookclone_#{Rails.env}"
-
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
