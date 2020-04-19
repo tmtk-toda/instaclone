779a7908051c27924cd7d7cf133745336e2fee99
tmtk <blindhurts.6001@gmail.com>
2020-04-19 18:19:42 +0900

    トップ画面修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :favorites
   resources :feeds
   get 'sessions/new'
-  root 'blogs#index'
+  root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :favorites, only: [:create, :destroy]
